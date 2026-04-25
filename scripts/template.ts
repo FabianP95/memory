4be4df5ecc620da2133bdb097b19614409ab5bbc
Fabian Puchta <fabianpuchta@hotmail.de>
2026-04-25 19:55:04 +0200

    feat: first draft of base html structure of settings page

---
+++ scripts/template.ts
@@ -0,0 +1,16 @@
+function landingPage(params: string) {
+    return `
+    <div class="content-wrapper">
+        <div class="start-header">
+            <h2>It's play time</h2>
+            <h1>Ready to play?</h1>
+        </div>
+        <button class="btn start-btn">
+            <img class="before-img" src="./public/assets/img/index/controllerBtn.svg" alt="small image of a controller">
+            <span>Play</span>
+            <img class="after-img" src="./public/assets/img/index/arrowDef.svg" alt="pointing arrow">
+        </button>
+    </div>
+    <img class="start-img" src="./public/assets/img/index/controllerBig.png" alt="big image of a controller">
+    `
+}
