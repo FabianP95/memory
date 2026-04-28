8cdb54b9d62089dfbbe4e7aeb27dcd975ecc0a8d
Fabian Puchta <fabianpuchta@hotmail.de>
2026-04-28 18:36:26 +0200

    feat: added first draft of html and css code for the game over html content and the result content
    refactor: - deleted unecessary file
    - renamed file
    - moved code to template ts file

--- scripts/template.ts
+++ scripts/template.ts
@@ -96,3 +96,45 @@ function settingPage(params: string){
     </div>
     `
 }
+
+function gameOverCode(params:string) {
+    return `
+    <div class="content-wrapper">
+        <h1 class="h1-go-c">Game Over</h1>
+        <span class="span-go-c">Final score</span>
+        <section class="scoreboard">
+            <div class="scoreboard__container">
+                <img src="./public/assets/img/game/results/code_theme/blue-label.png" alt="blue player label">
+                <span class="scoreboard__b-write">Blue <span id="resultBlue">16</span></span>
+            </div>
+            <div class="scoreboard__container">
+                <img src="./public/assets/img/game/results/code_theme/orange-label.png" alt="orange player label">
+                <span class="scoreboard__o-write">Orange <span id="resultOrange">16</span></span>
+            </div>
+        </section>
+    </div>`
+}
+
+function winnerCode(params:string) {
+    return `
+    <div class="content-wrapper pos">
+        <img class="header-img" src="./public/assets/img/game/results/code_theme/confetti.png"
+            alt="display of confetti">
+        <p class="winner-p ">The winner is</p>
+        <h2 class="h2-winner ">Orange Player</h2>
+        <img src="./public/assets/img/game/results/code_theme/pawn-big-orange.png" alt="">
+        <button class="btn back-btn">Back to start</button>
+    </div>`
+}
+
+function gameOverProjects(params:string) {
+    return `
+    
+    `
+}
+
+function winnerProjects(params:string) {
+    return `
+   
+    `
+}
